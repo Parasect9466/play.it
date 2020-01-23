a70cd65016ca3d6a55e728ee73cc23aa24076833
vv221 <vv221@dotslashplay.it>
2020-01-23 17:22:06 +0100

    New game: Vampire: The Masquerade - Redemption

---
+++ play.it-2/games/play-vampire-the-masquerade-redemption.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+set -o errexit
+
+###
+# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Vampire: The Masquerade - Redemption
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
+###
+
+script_version=20200123.1
+
+# Set game-specific variables
+
+GAME_ID='vampire-the-masquerade-redemption'
+GAME_NAME='Vampire: The Masquerade - Redemption'
+
+ARCHIVE_GOG='setup_vampire_the_masquerade_redemption_2.0.0.3.exe'
+ARCHIVE_GOG_URL='https://www.gog.com/game/vampire_the_masquerade_redemption'
+ARCHIVE_GOG_MD5='96c19b0ce487e53dd8fe24ef963d31fc'
+ARCHIVE_GOG_VERSION='1.0-gog2.0.0.3'
+ARCHIVE_GOG_SIZE='1300000'
+
+ARCHIVE_DOC_DATA_PATH='app'
+ARCHIVE_DOC_DATA_FILES='*.pdf *.rtf'
+
+ARCHIVE_GAME_BIN_PATH='app'
+ARCHIVE_GAME_BIN_FILES='*.dll *.exe *.inf *.isu extras/cs miles'
+
+ARCHIVE_GAME_DATA_PATH='app'
+ARCHIVE_GAME_DATA_FILES='*.dat *.nob *.pub *.zip vampireankh.ico chronicles extras sounds video'
+
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='vampire.exe'
+APP_MAIN_ICON='vampireankh.ico'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID wine"
+
+# Load common functions
+
+target_version='2.11'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Extract icons
+
+PKG='PKG_BIN'
+icons_get_from_package 'APP_MAIN'
+move_icons_to 'PKG_DATA'
+
+# Write launchers
+
+PKG='PKG_BIN'
+launchers_write 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
