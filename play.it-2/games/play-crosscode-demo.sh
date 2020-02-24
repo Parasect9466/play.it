a651575a25e21368c02931531fe2f5a8ce88247e
BetaRays <BetaRays+framagit@protonmail.com>
2020-02-24 12:30:51 +0100

    CrossCode Demo — Use firefox instead of NW.js binaries

--- play.it-2/games/play-crosscode-demo.sh
+++ play.it-2/games/play-crosscode-demo.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2019, Antoine "vv221/vv222" Le Gonidec
-# Copyright (c) 2018-2019, BetaRays
+# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2018-2020, BetaRays
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190615.1
+script_version=20200223.1
 
 # Set game-specific variables
 
@@ -51,41 +51,21 @@ ARCHIVE_CROSSCODE_VERSION='0.7.1beta-crosscode'
 ARCHIVE_CROSSCODE_SIZE='130000'
 ARCHIVE_CROSSCODE_TYPE='zip'
 
-ARCHIVE_OPTIONAL_NWJS_32='nwjs-v0.39.0-linux-ia32.tar.gz'
-ARCHIVE_OPTIONAL_NWJS_32_URL='https://nwjs.io/downloads/'
-ARCHIVE_OPTIONAL_NWJS_32_MD5='cba7aed7d2307e835a625ced8713e6d3'
-
-ARCHIVE_OPTIONAL_NWJS_64='nwjs-v0.39.0-linux-x64.tar.gz'
-ARCHIVE_OPTIONAL_NWJS_64_URL='https://nwjs.io/downloads/'
-ARCHIVE_OPTIONAL_NWJS_64_MD5='88d51da8f6ac54c0f3d7c213ee629fd2'
-
 ARCHIVE_DOC_WINE_PATH='.'
 ARCHIVE_DOC_WINE_FILES='credits.html'
 
 ARCHIVE_GAME_WINE_PATH='.'
 ARCHIVE_GAME_WINE_FILES='crosscode-demo.exe nw.pak ffmpegsumo.dll icudt.dll'
 
-ARCHIVE_DOC_MAIN_32_PATH='.'
-ARCHIVE_DOC_MAIN_32_FILES='nwjs-v0.39.0-linux-ia32/credits.html'
-
-ARCHIVE_GAME_MAIN_32_PATH='nwjs-v0.39.0-linux-ia32'
-ARCHIVE_GAME_MAIN_32_FILES='lib *.bin icudtl.dat resources.pak locales nw'
-
-ARCHIVE_DOC_MAIN_64_PATH='.'
-ARCHIVE_DOC_MAIN_64_FILES='nwjs-v0.39.0-linux-x64/credits.html'
-
-ARCHIVE_GAME_MAIN_64_PATH='nwjs-v0.39.0-linux-x64'
-ARCHIVE_GAME_MAIN_64_FILES='lib *.bin icudtl.dat resources.pak locales nw'
-
 APP_WINE_TYPE='wine'
 APP_WINE_EXE='crosscode-demo.exe'
 APP_WINE_ICON='favicon.png'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_EXE='nw'
+APP_MAIN_EXE='crosscode-web/node-webkit.html'
 APP_MAIN_ICON='favicon.png'
 
-PACKAGES_LIST='PKG_WINE'
+PACKAGES_LIST='PKG_WINE PKG_MAIN'
 
 # glx is here because it seems nw can use regular OpenGL when swiftshader isn't present
 
@@ -94,17 +74,10 @@ PKG_WINE_ARCH='32'
 PKG_WINE_PROVIDE="${GAME_ID}"
 PKG_WINE_DEPS='wine glx alsa'
 
-PKG_MAIN_32_ARCH='32'
-PKG_MAIN_32_DEPS='glx nss alsa glibc gtk2 xcursor libxrandr'
-PKG_MAIN_32_DEPS_DEB='' #TODO: fill
-PKG_MAIN_32_DEPS_ARCH='' #TODO: fill
-PKG_MAIN_32_DEPS_GENTOO='dev-libs/glib:2[abi_x86_32] dev-libs/nspr[abi_x86_32] app-accessibility/at-spi2-core[abi_x86_32] dev-libs/atk[abi_x86_32] dev-libs/expat[abi_x86_32] net-print/cups[abi_x86_32] sys-apps/dbus[abi_x86_32] sys-apps/util-linux[abi_x86_32] x11-libs/cairo[abi_x86_32] x11-libs/gdk-pixbuf[abi_x86_32] x11-libs/gtk+:3[X,abi_x86_32] x11-libs/libX11[abi_x86_32] x11-libs/libxcb[abi_x86_32] x11-libs/libXcomposite[abi_x86_32] x11-libs/libXdamage[abi_x86_32] x11-libs/libXext[abi_x86_32] x11-libs/libXfixes[abi_x86_32] x11-libs/libXi[abi_x86_32] x11-libs/libXrender[abi_x86_32] x11-libs/libXScrnSaver[abi_x86_32] x11-libs/libXtst[abi_x86_32] x11-libs/pango[abi_x86_32]'
 
-PKG_MAIN_64_ARCH='64'
-PKG_MAIN_64_DEPS='glx glibc nss alsa gtk2 xcursor libxrandr'
-PKG_MAIN_64_DEPS_DEB='' #TODO: fill
-PKG_MAIN_64_DEPS_ARCH='' #TODO: fill
-PKG_MAIN_64_DEPS_GENTOO='dev-libs/glib:2 dev-libs/nspr app-accessibility/at-spi2-core dev-libs/atk dev-libs/expat net-print/cups sys-apps/dbus sys-apps/util-linux x11-libs/cairo x11-libs/gdk-pixbuf x11-libs/gtk+:3[X] x11-libs/libX11 x11-libs/libxcb x11-libs/libXcomposite x11-libs/libXdamage x11-libs/libXext x11-libs/libXfixes x11-libs/libXi x11-libs/libXrender x11-libs/libXScrnSaver x11-libs/libXtst x11-libs/pango'
+PKG_MAIN_DEPS_DEB='firefox'
+PKG_MAIN_DEPS_ARCH='firefox'
+PKG_MAIN_DEPS_GENTOO='|| ( www-client/firefox www-client/firefox-bin )'
 
 # Load common functions
 
@@ -134,77 +107,53 @@ fi
 #shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
-# Use optional nw.js native executables
-
-ARCHIVE_MAIN="$ARCHIVE"
-archive_set 'ARCHIVE_NWJS_32' 'ARCHIVE_OPTIONAL_NWJS_32'
-if [ "$ARCHIVE_NWJS_32" ]; then
-	PACKAGES_LIST="$PACKAGES_LIST PKG_MAIN_32"
-	SCRIPT_DEPS="$SCRIPT_DEPS unzip"
-fi
-archive_set 'ARCHIVE_NWJS_64' 'ARCHIVE_OPTIONAL_NWJS_64'
-if [ "$ARCHIVE_NWJS_64" ]; then
-	PACKAGES_LIST="$PACKAGES_LIST PKG_MAIN_64"
-	SCRIPT_DEPS="$SCRIPT_DEPS unzip"
-fi
-ARCHIVE="$ARCHIVE_MAIN"
-[ "$ARCHIVE_NWJS_32" ] || [ "$ARCHIVE_NWJS_64" ] && {
-	select_package_architecture
-	# shellcheck disable=SC2086
-	set_temp_directories $PACKAGES_LIST
-	check_deps
-}
-
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-[ "$ARCHIVE_NWJS_32" ] && (
-	ARCHIVE='ARCHIVE_NWJS_32'
-	extract_data_from "$ARCHIVE_NWJS_32"
-)
-[ "$ARCHIVE_NWJS_64" ] && (
-	ARCHIVE='ARCHIVE_NWJS_64'
-	extract_data_from "$ARCHIVE_NWJS_64"
-)
 
 exe_path="$PLAYIT_WORKDIR/gamedata/crosscode-demo.exe"
-dd if="$exe_path" bs="$(unzip -l "$exe_path" 2>&1 1>/dev/null | grep --extended-regexp --only-matching '[0-9]+ extra bytes' | grep --extended-regexp --only-matching '^[0-9]+')" skip=1 of="$PLAYIT_WORKDIR/gamedata/crosscode-demo_exe.zip" status=none
+[ -d "$PLAYIT_WORKDIR/extraction" ] || mkdir "$PLAYIT_WORKDIR/extraction"
+dd if="$exe_path" bs="$(unzip -l "$exe_path" 2>&1 1>/dev/null | grep --extended-regexp --only-matching '[0-9]+ extra bytes' | grep --extended-regexp --only-matching '^[0-9]+')" skip=1 of="$PLAYIT_WORKDIR/extraction/crosscode-demo_exe.zip" status=none
 
-for PKG in $PACKAGES_LIST; do
-	if [ "$PKG" = 'PKG_WINE' ]; then
-		continue
-	fi
-	doc_dir="$(get_value "${PKG}_PATH")${PATH_DOC}/crosscode"
-	mkdir --parents "$doc_dir"
-	cp "$PLAYIT_WORKDIR/gamedata/credits.html" "$doc_dir/"
+doc_dir="${PKG_MAIN_PATH}${PATH_DOC}/crosscode"
+mkdir --parents "$doc_dir"
+cp "$PLAYIT_WORKDIR/gamedata/credits.html" "$doc_dir/"
 
-	cat "$PLAYIT_WORKDIR/gamedata/crosscode-demo_exe.zip" >> "$PLAYIT_WORKDIR/gamedata/$(get_value "ARCHIVE_GAME_${PKG#PKG_}_PATH")/nw"
-
-done
 prepare_package_layout
+
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+extract_data_from "$PLAYIT_WORKDIR/extraction/crosscode-demo_exe.zip"
 
 # Extract game icons
 
-extract_data_from "$PLAYIT_WORKDIR/gamedata/crosscode-demo_exe.zip"
-for PKG in $PACKAGES_LIST; do
-	if [ "$PKG" = 'PKG_WINE' ]; then
-		icons_get_from_workdir 'APP_WINE'
-		continue
-	fi
-	icons_get_from_workdir 'APP_MAIN'
-done
+PKG='PKG_WINE'
+icons_get_from_workdir 'APP_WINE'
+PKG='PKG_MAIN'
+icons_get_from_workdir 'APP_MAIN'
+
+# Fix node-webkit.html to work with regular web browsers
+
+pattern="/    \/\/ make sure we don't let node-webkit show it's error page/,+8d"
+sed --regexp-extended --in-place --expression "$pattern" "$PLAYIT_WORKDIR/gamedata/node-webkit.html"
+
+# Copy game "website" for main package
+
+game_path="${PKG_MAIN_PATH}${PATH_GAME}"
+mkdir --parents "$game_path/crosscode-web"
+cp --recursive "$PLAYIT_WORKDIR/gamedata/"* "$game_path/crosscode-web"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-for PKG in $PACKAGES_LIST; do
-	if [ "$PKG" = 'PKG_WINE' ]; then
-		launcher_write 'APP_WINE'
-		continue
-	fi
-	launcher_write 'APP_MAIN'
-done
+PKG='PKG_WINE'
+launcher_write 'APP_WINE'
+PKG='PKG_MAIN'
+launcher_write 'APP_MAIN'
+
+pattern='s/^".\/\$APP_EXE"/firefox --kiosk &/'
+sed --regexp-extended --in-place --expression "$pattern" "$(get_value "${PKG}_PATH")/${PATH_BIN}/$GAME_ID"
 
 # Build package
 
@@ -217,7 +166,7 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-#TODO: separate wine with 32bits and 64bits
+#TODO: separate wine and main
 print_instructions
 
 exit 0
