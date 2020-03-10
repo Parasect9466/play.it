7971be43c52a5bd3cc9577a9fe9c76a574ddfacb
vv221 <vv221@dotslashplay.it>
2020-03-10 10:29:21 +0100

    Allow to extract .deb files without dpkg (fixup)
    Move extraction logic out of the dependencies-checking section of the
    sources

--- play.it-2/src/20_dependencies.sh
+++ play.it-2/src/20_dependencies.sh
@@ -60,7 +60,7 @@ check_deps() {
 				check_deps_innoextract "$dep"
 			;;
 			('debian')
-				check_deps_deb
+				check_deps_debian
 			;;
 			(*)
 				if ! command -v "$dep" >/dev/null 2>&1; then
@@ -82,6 +82,26 @@ check_deps_7z() {
 		fi
 	done
 	error_dependency_not_found '7zr'
+}
+
+# check presence of a software to handle .deb packages
+# USAGE: check_deps_debian
+# CALLS: error_dependency_not_found
+# CALLED BY: check_deps
+check_deps_debian() {
+	for command in 'dpkg-deb' 'bsdtar' 'unar'; do
+		if command -v "$command" >/dev/null 2>&1; then
+			return 0
+		fi
+	done
+	if command -v 'tar' >/dev/null 2>&1; then
+		for command in '7z' '7zr' 'ar'; do
+			if command -v "$command" >/dev/null 2>&1; then
+				return 0
+			fi
+		done
+	fi
+	error_dependency_not_found 'dpkg-deb'
 }
 
 # check innoextract presence, optionally in a given minimum version
@@ -138,53 +158,14 @@ dependency_provided_by() {
 		('convert'|'identify')
 			provider='imagemagick'
 		;;
+		('dpkg-deb')
+			provider='dpkg'
+		;;
 		(*)
 			provider="$command"
 		;;
 	esac
 	printf '%s' "$provider"
 	return 0
-}
-
-# check presence of a software to handle .deb archives
-# USAGE: check_deps_deb
-# CALLS: check_deps_error_not_found
-# CALLED BY: check_deps
-check_deps_deb() {
-	if command -v dpkg-deb >/dev/null 2>&1; then
-		extract_deb() { dpkg-deb --extract "$1" "$2"; }
-	elif command -v bsdtar >/dev/null 2>&1; then
-		extract_deb() { bsdtar --extract --to-stdout --file "$1" 'data*' | bsdtar --directory "$2" --extract --file /dev/stdin; }
-	elif command -v unar >/dev/null 2>&1; then
-		extract_deb() {
-			[ -d "$PLAYIT_WORKDIR/extraction" ] || mkdir "$PLAYIT_WORKDIR/extraction"
-			unar -output-directory "$PLAYIT_WORKDIR/extraction" -force-overwrite -no-directory "$1" 'data*' 1>/dev/null
-			unar -output-directory "$2" -force-overwrite -no-directory "$PLAYIT_WORKDIR/extraction"/data* 1>/dev/null
-			rm --recursive --force "$PLAYIT_WORKDIR/extraction"
-		}
-	elif command -v tar >/dev/null 2>&1; then
-		if command -v 7z >/dev/null 2>&1; then
-			extract_deb() { 7z x -i'!data*' -so "$1" | tar --directory "$2" --extract; }
-		elif command -v 7zr >/dev/null 2>&1; then
-			extract_deb() { 7zr x -so "$1" | tar --directory "$2" --extract; }
-		elif command -v ar >/dev/null 2>&1; then
-			extract_deb() {
-				[ -d "$PLAYIT_WORKDIR/extraction" ] || mkdir "$PLAYIT_WORKDIR/extraction"
-				local archive_absolute_path
-				archive_absolute_path="$(realpath --canonicalize-existing "$1")"
-				(
-					cd "$PLAYIT_WORKDIR/extraction"
-					ar x "$archive_absolute_path" "$(ar t "$archive_absolute_path" | grep ^data)"
-
-				)
-				tar --directory "$2" --extract --file "$PLAYIT_WORKDIR/extraction"/data*
-				rm --recursive --force "$PLAYIT_WORKDIR/extraction"
-			}
-		else
-			check_deps_error_not_found 'dpkg-deb/bsdtar/unar/p7zip/ar'
-		fi
-	else
-		check_deps_error_not_found 'dpkg-deb/bsdtar/unar/tar'
-	fi
 }
 
