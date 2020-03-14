6b21c80856a94cd1220229c0b3567c8e0702441a
macaron <23-macaron@users.noreply.forge.dotslashplay.it>
2020-03-14 12:10:56 +0100

    Monkey Island 4 — Add support for ResidualVM version

--- play.it-2/games/play-monkey-island-4.sh
+++ play.it-2/games/play-monkey-island-4.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200314.1
+script_version=20200314.3
 
 # Set game-specific variables
 
@@ -62,28 +62,43 @@ ARCHIVE_GOG_FR_PART1='setup_escape_from_monkey_islandtm_1.1_(french)_(20987)-1.b
 ARCHIVE_GOG_FR_PART1_TYPE='innosetup'
 ARCHIVE_GOG_FR_PART1_MD5='c5bf233f09cca2a8e33d78d25cf58329'
 
+ARCHIVE_RESIDUALVM_PATCH_EN='MonkeyUpdate.exe'
+ARCHIVE_RESIDUALVM_PATCH_EN_URL='https://demos.residualvm.org/patches/'
+ARCHIVE_RESIDUALVM_PATCH_EN_MD5='7c7dbd2349d49e382a2dea40bed448e0'
+ARCHIVE_RESIDUALVM_PATCH_EN_TYPE='file'
+
+ARCHIVE_RESIDUALVM_PATCH_FR='MonkeyUpdate_FRA.exe'
+ARCHIVE_RESIDUALVM_PATCH_FR_URL='https://demos.residualvm.org/patches/'
+ARCHIVE_RESIDUALVM_PATCH_FR_MD5='cc5ff3bb8f78a0eb4b8e0feb9cdd2e87'
+ARCHIVE_RESIDUALVM_PATCH_FR_TYPE='file'
+
 ARCHIVE_DOC_DATA_PATH='.'
 ARCHIVE_DOC_DATA_FILES='*.pdf *.txt'
 
-ARCHIVE_GAME_BIN_PATH='.'
-ARCHIVE_GAME_BIN_FILES='install *.asi *.dll *.exe *.flt'
+ARCHIVE_GAME0_BIN_WINE_PATH='.'
+ARCHIVE_GAME0_BIN_WINE_FILES='install *.asi *.dll *.exe *.flt'
 
-ARCHIVE_GAME0_DATA_PATH='.'
-ARCHIVE_GAME0_DATA_FILES='movies textures *.m4b'
+ARCHIVE_GAME1_BIN_WINE_PATH='__support/save'
+ARCHIVE_GAME1_BIN_WINE_FILES='saves'
 
-ARCHIVE_GAME1_DATA_PATH='__support/save'
-ARCHIVE_GAME1_DATA_FILES='saves'
+ARCHIVE_GAME_BIN_RESIDUALVM_PATH='.'
+ARCHIVE_GAME_BIN_RESIDUALVM_FILES='MonkeyUpdate*.exe'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='movies textures *.m4b'
 
 CONFIG_FILES='saves/efmi.cfg'
 DATA_FILES='saves/efmi*.gsv'
 
 APP_REGEDIT='install.reg'
 
-APP_MAIN_TYPE='wine'
+APP_MAIN_TYPE_BIN_WINE='wine'
+APP_MAIN_TYPE_BIN_RESIDUALVM='residualvm'
 APP_MAIN_EXE='monkey4.exe'
+APP_MAIN_RESIDUALID='monkey4'
 APP_MAIN_ICON='monkey4.exe'
 
-PACKAGES_LIST='PKG_BIN PKG_DATA'
+PACKAGES_LIST='PKG_BIN_WINE PKG_BIN_RESIDUALVM PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_ID_GOG_EN="${PKG_DATA_ID}-en"
@@ -93,19 +108,25 @@ PKG_DATA_DESCRIPTION='data'
 PKG_DATA_DESCRIPTION_GOG_EN="$PKG_DATA_DESCRIPTION - English version"
 PKG_DATA_DESCRIPTION_GOG_FR="$PKG_DATA_DESCRIPTION - French version"
 
-PKG_BIN_ARCH='32'
 PKG_BIN_ID="$GAME_ID"
-PKG_BIN_ID_GOG_EN="${PKG_BIN_ID}-en"
-PKG_BIN_ID_GOG_FR="${PKG_BIN_ID}-fr"
-PKG_BIN_PROVIDE="$PKG_BIN_ID"
-PKG_BIN_DEPS_GOG_EN="${PKG_DATA_ID}-en wine"
-PKG_BIN_DEPS_GOG_FR="${PKG_DATA_ID}-fr wine"
-PKG_BIN_DESCRIPTION_GOG_EN='English version'
-PKG_BIN_DESCRIPTION_GOG_FR='French version'
+
+PKG_BIN_WINE_ID="${PKG_BIN_ID}-wine"
+PKG_BIN_WINE_ARCH='32'
+PKG_BIN_WINE_ID_GOG_EN="${PKG_BIN_WINE_ID}-en"
+PKG_BIN_WINE_ID_GOG_FR="${PKG_BIN_WINE_ID}-fr"
+PKG_BIN_WINE_PROVIDE="$PKG_BIN_ID"
+PKG_BIN_WINE_DEPS_GOG_EN="${PKG_DATA_ID}-en wine"
+PKG_BIN_WINE_DEPS_GOG_FR="${PKG_DATA_ID}-fr wine"
+PKG_BIN_WINE_DESCRIPTION_GOG_EN='English version'
+PKG_BIN_WINE_DESCRIPTION_GOG_FR='French version'
+
+PKG_BIN_RESIDUALVM_ID="${PKG_BIN_ID}-residualvm"
+PKG_BIN_RESIDUALVM_PROVIDE="$PKG_BIN_ID"
+PKG_BIN_RESIDUALVM_DEPS="$PKG_DATA_ID residualvm"
 
 # Load common functions
 
-target_version='2.11'
+target_version='2.12'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
@@ -131,21 +152,42 @@ fi
 # shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
+# Check presence of patch required by ResidualVM
+
+ARCHIVE_MAIN="$ARCHIVE"
+case "$ARCHIVE_MAIN" in
+	('ARCHIVE_GOG_EN')
+		ARCHIVE_PATCH='ARCHIVE_RESIDUALVM_PATCH_EN'
+	;;
+	('ARCHIVE_GOG_FR')
+		ARCHIVE_PATCH='ARCHIVE_RESIDUALVM_PATCH_FR'
+	;;
+esac
+archive_set 'ARCHIVE_RESIDUALVM_PATCH' "$ARCHIVE_PATCH"
+if [ -z "$ARCHIVE_RESIDUALVM_PATCH" ]; then
+	PACKAGES_LIST='PKG_BIN_WINE PKG_DATA'
+fi
+ARCHIVE="$ARCHIVE_MAIN"
+
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
+if [ -n "$ARCHIVE_RESIDUALVM_PATCH" ]; then
+	cp "$ARCHIVE_RESIDUALVM_PATCH" "$PLAYIT_WORKDIR/gamedata"
+	prepare_package_layout 'PKG_BIN_RESIDUALVM'
+fi
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Extract icons
 
-PKG='PKG_BIN'
+PKG='PKG_BIN_WINE'
 icons_get_from_package 'APP_MAIN'
 icons_move_to 'PKG_DATA'
 
 # Register install path
 
-cat > "${PKG_BIN_PATH}${PATH_GAME}/install.reg" << EOF
+cat > "${PKG_BIN_WINE_PATH}${PATH_GAME}/install.reg" << EOF
 Windows Registry Editor Version 5.00
 
 [HKEY_LOCAL_MACHINE\\Software\\LucasArts Entertainment Company LLC\\Monkey4\\Retail]
@@ -154,8 +196,15 @@ EOF
 
 # Write launchers
 
-PKG='PKG_BIN'
+PKG='PKG_BIN_WINE'
+use_package_specific_value 'APP_MAIN_TYPE'
 launchers_write 'APP_MAIN'
+
+if [ -n "$ARCHIVE_RESIDUALVM_PATCH" ]; then
+	PKG='PKG_BIN_RESIDUALVM'
+	use_package_specific_value 'APP_MAIN_TYPE'
+	launchers_write 'APP_MAIN'
+fi
 
 # Build package
 
@@ -168,6 +217,12 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-print_instructions
+printf '\n'
+if [ -n "$ARCHIVE_RESIDUALVM_PATCH" ]; then
+	printf 'ResidualVM:'
+	print_instructions 'PKG_DATA' 'PKG_BIN_RESIDUALVM'
+fi
+printf 'WINE:'
+print_instructions 'PKG_DATA' 'PKG_BIN_WINE'
 
 exit 0
