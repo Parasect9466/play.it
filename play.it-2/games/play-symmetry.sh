afe3b98f81372cf9286c7b01428ef7cbe03d6f50
vv221 <vv221@dotslashplay.it>
2020-04-22 00:09:34 +0200

    New game: Symmetry
    Thanks to bbob for the contribution ;)

---
+++ play.it-2/games/play-symmetry.sh
@@ -0,0 +1,139 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2019, Antoine Le Gonidec
+# Copyright (c) 2018-2019, BetaRays
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# SYMMETRY
+# build native packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20200421.1
+
+# Set game-specific variables
+
+GAME_ID='symmetry'
+GAME_NAME='SYMMETRY'
+
+ARCHIVE_GOG='setup_symmetry_1.0.2.win_(21945).exe'
+ARCHIVE_GOG_URL='https://www.gog.com/game/symmetry'
+ARCHIVE_GOG_MD5='2a0d1919d41b48b8dc5af3071ad7babc'
+ARCHIVE_GOG_SIZE='210000'
+ARCHIVE_GOG_VERSION='1.0.2-gog21945'
+ARCHIVE_GOG_TYPE='innosetup'
+
+ARCHIVE_GAME_BIN_PATH='.'
+ARCHIVE_GAME_BIN_FILES='symmetry.exe d3dx9_43.dll cchromaeditorlibrary.dll'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='*.ogg *.chroma *.ttf *.csv *.ini *.png'
+
+APP_WINETRICKS='d3dcompiler_43'
+
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='symmetry.exe'
+APP_MAIN_ICON='symmetry.exe'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID wine winetricks glx"
+
+# Load common functions
+
+target_version='2.11'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+(
+	ARCHIVE='INNER_ARCHIVE'
+	mv "$PLAYIT_WORKDIR/gamedata/symmetry.exe" "$PLAYIT_WORKDIR/gamedata/symmetry_cab.exe"
+	INNER_ARCHIVE="$PLAYIT_WORKDIR/gamedata/symmetry_cab.exe"
+	INNER_ARCHIVE_TYPE='cabinet'
+	extract_data_from "$INNER_ARCHIVE"
+	rm "$INNER_ARCHIVE"
+)
+
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Get icon
+
+PKG='PKG_BIN'
+icons_get_from_package 'APP_MAIN'
+icons_move_to 'PKG_DATA'
+
+# Write launchers
+
+PKG='PKG_BIN'
+launcher_write 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
